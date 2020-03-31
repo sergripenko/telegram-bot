42618b40f5fff6473c3d846f82d35606243fb804
Sergey Ripenko <ripenko.s@udtech.co>
2020-03-31 11:02:48 +0300

    update

--- models/base.go
+++ models/base.go
@@ -5,5 +5,5 @@ import "time"
 type Base struct {
 	CreatedAt time.Time `orm:"column(created_at);auto_now_add;type(timestamp with time zone);null" json:"created_at"`
 	UpdatedAt time.Time `orm:"column(updated_at);auto_now;type(timestamp with time zone);null" json:"updated_at"`
-	DeletedAt time.Time `orm:"column(deleted_at);auto_now;type(timestamp with time zone);null" json:"deleted_at"`
+	DeletedAt time.Time `orm:"column(deleted_at);type(timestamp with time zone);null" json:"deleted_at"`
 }
